8844e171c25f956a48f7d079a91067d0994dea4a
Michael Kardijian <makardjian@gmail.com>
2021-12-29 13:54:35 -0800

    save progress using Card and CardMedia

--- src/components/LandingPageMiddle/LandingPageMiddle.tsx
+++ src/components/LandingPageMiddle/LandingPageMiddle.tsx
@@ -1,18 +1,72 @@
 import React from 'react';
-import useStyles from './LandingPageMiddle.styles';
+import { Card, CardMedia, Box } from '@mui/material';
 import inputBackgroundImage from '../../static/images/bg-shorten-desktop.svg';
+//
+import { makeStyles } from '@mui/styles';
+import Colors from '../../styles/Colors';
+
+const useStyles = makeStyles({
+  contentContainer: {
+    height: '1000px',
+    paddingTop: '100px',
+    display: 'flex',
+    justifyContent: 'center',
+    flexDirection: 'column',
+  },
+  card: {
+    position: 'relative',
+  },
+  inputBackgroundImage: {
+    borderRadius: '10px',
+    backgroundColor: Colors.primary['@darkViolet'],
+  },
+  cardMediaContainer: {
+    display: 'flex',
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 const LandingPageMiddle = () => {
   const styles = useStyles();
   return (
     <div className={styles.contentContainer}>
-      <div>
-        <img
-          className={styles.inputBackgroundImage}
-          src={inputBackgroundImage}
-          alt='input_background_img'
+      <Card
+        sx={{
+          position: 'relative',
+          height: '200px',
+          borderRadius: '10px',
+        }}
+      >
+        <CardMedia
+          image={inputBackgroundImage}
+          sx={{
+            height: '100%',
+            backgroundColor: Colors.primary['@darkViolet'],
+          }}
         />
-      </div>
+        <Box
+          sx={{
+            position: 'absolute',
+            bottom: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            color: 'white',
+          }}
+        >
+          <div className={styles.cardMediaContainer}>
+            <div>hello world</div>
+          </div>
+        </Box>
+      </Card>
+
+      <img
+        className={styles.inputBackgroundImage}
+        src={inputBackgroundImage}
+        alt='input_background_img'
+      />
     </div>
   );
 };
