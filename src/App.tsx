83b764ed1db1614ce967ee31f6c2b9c89acd346e
Michael Kardijian <makardjian@gmail.com>
2021-12-30 18:15:42 -0800

    save progresss on api fetching and data handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navigation from './components/Navigation/Navigation';
 import LandingPageTop from './components/LandingPageTop/LandingPageTop';
 import LinkShortener from './components/LinkShortner/LinkShortener';
-import { fetchShortenedLink } from './api/fetchShortenedLink';
+import {
+  fetchShortenedLink,
+  TransformedApiResponse,
+} from './api/fetchShortenedLink';
 import useStyles from './App.styles';
 
 function App() {
   const classes = useStyles();
+  const [links, setLinks] = useState([] as string[]);
+  const [linkGenerationError, setLinkGenerationError] = useState('');
 
   const generateShortenedLink = async (originalUrl: string) => {
-    const response = await fetchShortenedLink(originalUrl);
-    return;
+    const response: TransformedApiResponse = await fetchShortenedLink(
+      originalUrl
+    );
+    const { shortLink, errorMessage } = response;
+    if (shortLink) {
+      const newLinks = [...links, shortLink];
+      setLinks(newLinks);
+      return;
+    }
+    if (errorMessage) {
+      setLinkGenerationError(errorMessage);
+    }
   };
+
+  console.log({
+    links,
+    linkGenerationError,
+  });
 
   return (
     <div className={classes.appRoot}>
--- src/api/fetchShortenedLink.ts
+++ src/api/fetchShortenedLink.ts
@@ -1,13 +1,45 @@
 const API_BASE = 'https://api.shrtco.de/v2/shorten';
 
-export const fetchShortenedLink = async (originalUrl: string) => {
+type ApiResponse = {
+  ok: boolean;
+  result?: {
+    code: string;
+    full_share_link: string;
+    full_short_link: string;
+    full_short_link2: string;
+    full_short_link3: string;
+    original_link: string;
+    share_link: string;
+    short_link: string;
+    short_link2: string;
+    short_link3: string;
+  };
+  error?: string;
+  error_code?: number;
+};
+
+export type TransformedApiResponse = {
+  shortLink?: string;
+  errorMessage?: string;
+};
+
+export const fetchShortenedLink = async (
+  originalUrl: string
+): Promise<TransformedApiResponse> => {
   const REQUEST_URL = `${API_BASE}?url=${originalUrl}`;
   console.log({ REQUEST_URL });
   try {
     const response = await fetch(REQUEST_URL);
-    const responseData = await response.json();
-    console.log({ responseData });
-  } catch (e) {
-    console.log('oh no, an error happened');
+    const responseData: ApiResponse = await response.json();
+    if (responseData.ok) {
+      return {
+        shortLink: responseData?.result?.short_link,
+      };
+    }
+    throw new Error(responseData.error);
+  } catch (error: any) {
+    return {
+      errorMessage: error.message,
+    };
   }
 };
