53bf3375813b718aa0a9e1da94477b5518d0020c
Michael Kardijian <makardjian@gmail.com>
2022-01-17 21:20:16 -0800

    modify mock data

--- src/constants/mockData.ts
+++ src/constants/mockData.ts
@@ -6,17 +6,17 @@ export const MOCK_LINKS = [
     id: '23bdbfdc-5888-43be-82d6-2cc4c1d1a7b7',
   },
   {
-    shortLink: 'https://shrtco.de/GIFUah',
+    shortLink: 'https://shrtco.de/BFVuVh',
     originalLink:
-      'https://www.google.com/search?q=how to read system clipboard',
+      'https://www.bbc.co.uk/food/recipes/mary_berrys_lasagne_al_16923',
     isCopied: false,
     id: 'e4cecc4b-bcc6-49ca-8bce-98169cae173f',
   },
   {
-    id: '114f9a76-5722-4a38-acd8-3d0c9f2f2a57',
-    isCopied: false,
+    shortLink: 'https://shrtco.de/kZw65B',
     originalLink:
-      'https://www.google.com/search?q=execute callback when user copies text to clipboard',
-    shortLink: 'https://shrtco.de/PpXy3P',
+      'https://www.amazon.co.uk/Hello-World-How-Human-Machine/dp/0857525247',
+    isCopied: false,
+    id: '114f9a76-5722-4a38-acd8-3d0c9f2f2a57',
   },
 ];
